a18f2433994986eb46b2ba5746767f4b4584b0a5
naeemmuhammad <naeem.muhammad@libis.kuleuven.be>
2018-07-31 14:19:32 +0200

    utf8 encoding for result pdf print

--- app/printTemplates/results/pdfStart.php
+++ app/printTemplates/results/pdfStart.php
@@ -34,6 +34,7 @@
 ?><!DOCTYPE html>
 <html>
 	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 		<title><?php print $this->getVar('criteria_summary_truncated'); ?></title>
 		
 <?php
