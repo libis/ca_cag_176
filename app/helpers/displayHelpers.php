173f4ff94cdd8a95a41d14025562da3e3c2dc289
naeemmuhammad <naeem.muhammad@libis.kuleuven.be>
2018-08-30 15:01:09 +0200

    object image inspector left navigation

--- app/helpers/displayHelpers.php
+++ app/helpers/displayHelpers.php
@@ -1014,13 +1014,22 @@ require_once(__CA_LIB_DIR__.'/core/Media/MediaInfoCoder.php');
 					$vn_r++;
 				}
 
-					if (sizeof($va_reps) > 1) {
+					/*if (sizeof($va_reps) > 1) {
 						$vs_buf .= "
 					<div class='leftScroll'>
 						<a href='#' onclick='inspectorInfoRepScroller.scrollToPreviousImage(); return false;'>".caNavIcon(__CA_NAV_ICON_SCROLL_LT__, '16px')."</a>
 					</div>
 		";
-					}
+					}*/
+                }
+
+                if (sizeof($va_reps) > 1 || sizeof($imagePids) > 1) {
+                    $test = 10;
+                    $vs_buf .= "
+					<div class='leftScroll'>
+						<a href='#' onclick='inspectorInfoRepScroller.scrollToPreviousImage(); return false;'>".caNavIcon(__CA_NAV_ICON_SCROLL_LT__, '16px')."</a>
+					</div>
+		";
                 }
                 //libis_end
 									
